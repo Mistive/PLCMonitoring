323edad2834645bfd641d0d072e42c99c7a3d930
mistive <pms9725@gmail.com>
2020-04-09 10:41:07 +0900

    Find

--- communication.py
+++ communication.py
@@ -1,10 +1,10 @@
 import threading, time
 from PySide2.QtCore import *
-
+#https://github.com/RavenKyu/OpenTutorials_PyQt/blob/master/QtFramework/QtWidgets/QProgressBar/QProgressBar_01_thread.py
 
 class signalThread(QThread):
     finished = Signal(dict) #singal을 통해 어떤 인자를 보낼지 타입을 결정해야 한다. -> 안하면 에러 발생
-
+    syncTest = 0
     def run(self):
         while True:
             with threading.Lock():
@@ -19,13 +19,14 @@ class signalThread(QThread):
                 signalThread.syncTest += 1
                 print(signalThread.syncTest)
             return 1
-        except:
-            return None
+        except Exception as e:
+           print(e)
 
     @Slot()
-    def sendSignal(self, data):
+    def sendSignal(self, data, thread):
         with threading.Lock():
             print("Send: ", data)
+
 
 
 
--- monitor.py
+++ monitor.py
@@ -11,25 +11,21 @@ signal/slot 기능을 활용하여 community.py의 thread에서 연산이 완료
 '''
 class Monitor(QMainWindow, Ui_monitor):
 
+
     def __init__(self):
         super(Monitor, self).__init__()
         self.setupUi(self)
 
         self.sig = signalThread()
         self.sig.start()
-
-        #통신을 통해 한 스캔을 완료했을 경우 발생하는 signal이 finished이고, 해당 시그널이 발생하면 통신 결과를 Gui에 바로 반영할 수 있도록 update_info()를 호출
-        #그리고 update_info()의 인자로 받아온 값들을 통해 gui에 바로 업데이트를 할 수 있다.
         self.sig.finished.connect(self.update_info)
+
+
 
         #click을 눌렀을 경우 데이터를 communication.py에 보내기
         data = {}
         data['send'] = 'Good'
-        self.buttonIn.clicked.connect(lambda: self.sig.sendSignal(data))
-
-
-
-
+        self.buttonIn.clicked.connect(lambda: self.sig.sendSignal(data, self.sig))
 
 
     @Slot(dict)
